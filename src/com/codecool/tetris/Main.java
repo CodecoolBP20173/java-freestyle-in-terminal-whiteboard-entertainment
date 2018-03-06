6f7a203a94793377a736022b5ccd7e746b65ab02
Balint Bakcsa <balintbakcsa@gmail.com>
2018-03-06 15:37:50 +0100

    Compile/run bash script

---
+++ src/com/codecool/tetris/Main.java
@@ -0,0 +1,14 @@
+package com.codecool.tetris;
+
+import com.codecool.termlib.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Terminal a = new Terminal();
+        a.setBgColor(BGColor.CYAN);
+        a.setColor(Color.BLUE);
+        System.out.println("asdsad");
+        a.clearScreen();
+    }
+}
