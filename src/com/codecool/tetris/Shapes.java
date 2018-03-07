344829d11a43c82b44f6eca9a2bdb8573bbb9a7e
Alex <lisztes.alex@gmail.com>
2018-03-07 13:26:50 +0100

    Refactoring to map and making classes static

--- src/com/codecool/tetris/Shapes.java
+++ src/com/codecool/tetris/Shapes.java
@@ -1,9 +1,54 @@
 package com.codecool.tetris;
 
+import java.util.HashMap;
+import java.util.Map;
 import com.codecool.termlib.*;
+
 public class Shapes {
 
-    public abstract class Shape {
+    private static final Map<String, TerminalDirection[]> shapeForms = fillMap();
+
+    private static Map<String, TerminalDirection[]> fillMap(){
+        Map<String, TerminalDirection[]> m = new HashMap<>();
+        m.put("Z", new TerminalDirection[] {
+            TerminalDirection.RIGHT,
+            TerminalDirection.DOWN,
+            TerminalDirection.RIGHT
+        });
+        m.put("Z_mirrored", new TerminalDirection[] {
+            TerminalDirection.RIGHT,
+            TerminalDirection.UP,
+            TerminalDirection.RIGHT
+        });
+        m.put("T", new TerminalDirection[] {
+            TerminalDirection.RIGHT,
+            TerminalDirection.DOWN,
+            TerminalDirection.UPRIGHT
+        });
+        m.put("L", new TerminalDirection[] {
+            TerminalDirection.RIGHT,
+            TerminalDirection.UPLEFT,
+            TerminalDirection.UP
+        });
+        m.put("L_mirrored", new TerminalDirection[] {
+            TerminalDirection.RIGHT,
+            TerminalDirection.UP,
+            TerminalDirection.UP
+        });
+        m.put("I", new TerminalDirection[] {
+            TerminalDirection.UP,
+            TerminalDirection.UP,
+            TerminalDirection.UP
+        });
+        m.put("Square", new TerminalDirection[] {
+            TerminalDirection.UP,
+            TerminalDirection.RIGHT,
+            TerminalDirection.DOWN
+        });
+        return m;
+    }
+
+    public static abstract class Shape {
 
         private Coord coords;
         private BGColor color;
@@ -36,33 +81,21 @@ public class Shapes {
         }
     }
 
-    public class Square extends Shape {
+    public static class Square extends Shape {
         Square(int x, int y, BGColor color){
-            super(x, y, color, new TerminalDirection[] {
-                TerminalDirection.UP,
-                TerminalDirection.RIGHT,
-                TerminalDirection.DOWN
-            });
+            super(x, y, color, shapeForms.get("Square"));
         }
     }
 
-    public class ZShape extends Shape {
-        ZShape(int x, int y, BGColor color){
-            super(x, y, color, new TerminalDirection[] {
-                TerminalDirection.RIGHT,
-                TerminalDirection.UP,
-                TerminalDirection.RIGHT
-            });
+    public static class ZShape extends Shape {
+        ZShape(int x, int y, BGColor color, boolean mirr){
+            super(x, y, color, mirr? shapeForms.get("Z_mirrored") : shapeForms.get("Z"));
         }
     }
 
-    public class TShape extends Shape {
+    public static class TShape extends Shape {
         TShape(int x, int y, BGColor color){
-            super(x, y, color, new TerminalDirection[] {
-                TerminalDirection.RIGHT,
-                TerminalDirection.DOWN,
-                TerminalDirection.UPRIGHT
-            });
+            super(x, y, color, shapeForms.get("T"));
         }
     }
 }
