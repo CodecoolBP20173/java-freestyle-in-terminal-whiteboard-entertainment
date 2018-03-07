3d82f0797a4eacf91df7c96e9c9e1f8c13aec643
Tibor Vlasics <tibor.vlasics@gmail.com>
2018-03-07 09:53:56 +0100

    Coord class changeCoords method

--- src/com/codecool/tetris/Coord.java
+++ src/com/codecool/tetris/Coord.java
@@ -10,10 +10,23 @@ public class Coord {
     Coord (int x, int y) {
         this.X = x;
         this.Y = y;
-    
     }
     
     public int[] getCoord() {
+
         return new int[] {this.X, this.Y};
     }
+
+    public void changeCoords(Direction direction) {
+        switch (direction) {
+            case Direction.DOWN: this.Y -= 1;
+            break;
+            case Direction.RIGHT: this.X += 1;
+            break;
+            case Direction.LEFT: this.X -= 1;
+            break;
+        }
+    }
+
+
 }
